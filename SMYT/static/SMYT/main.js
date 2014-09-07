1168d6413eef8aaf6386c4e2ec9a1ae8705bf935
unknown <skhairullina@atc14-66.at-consulting.ru>
2014-09-07 22:38:21 +0400

    Init table js

--- SMYT/static/SMYT/main.js
+++ SMYT/static/SMYT/main.js
@@ -5,11 +5,17 @@
     $(function(){
         var maintable = $("#maintable");
 
-        var listModelObject = new listModelConstr($("#list-model"), [
+        var listModelObject = new listModelConstr($("#list-model"), {records: [
             {title: 'Пользователи', id: 'users'},
             {title: 'Комнаты', id: 'rooms'}
-        ]);
+        ]});
 
+        var tableModelObject = new tableModelConstr(maintable, {fields: [
+            {type: 'int', id: "aaaaa", title: 'Имя'},
+            {type: 'char', id: "bbbbb", title: 'Зарплата'},
+            {type: 'date', id: "ccccc", title: 'Дата поступления на работу'}
+
+        ]});
 
         create_table_from_structure(maintable, {
             fields: [
@@ -25,20 +31,70 @@
         });
     });
 
-    function listModelConstr(idel, listModels) {
-        var ulList = idel;
-        var models = listModels;
-        var currentItm = listModels[0];
-        render(models);
+    function listModelConstr(idel, initConfig) {
+        this.ulList = idel;
+        this.config = initConfig;
+        this.listModel = initConfig['records'];
 
-        var render = function(models) {
+        this.initFunc = function() {
+            this.render(this.listModel);
+        };
+
+        this.render = function(models) {
+            this.ulList.empty();
             var ul = $("<ul>");
             for(var i = 0; i < models.length; i++) {
-                ul.append($('<li>').text(models[i]['title']));
+                var name = models[i]['id'];
+                var li = $('<li>').text(models[i]['title']);
+                (function(name) {
+                    li.on('click', function(event) {
+                        console.log(name);
+                    });
+                })(name);
+
+                ul.append(li);
             }
-            ulList.append(ul);
+            this.ulList.append(ul);
+        };
+
+        this.initFunc();
+
+        return {
+
         }
     }
+
+    var tableModelConstr = function(ids, initConfig) {
+        this.table = ids;
+        this.config = initConfig;
+
+        
+
+        this.create_table_from_structure = function(table, structure) {
+        table.empty();
+
+        var fields = [];
+        var tr = $('<tr>');
+        table.append($('<thead>')
+            .append(tr));
+        for(var i = 0; i < structure['fields'].length; i++) {
+            fields.push(structure['fields'][i]['id']);
+            tr.append($('<td>').append(
+                $('<div>').text(structure['fields'][i]['title'])));
+        }
+
+        for(var i = 0; i < structure['records'].length; i++) {
+            tr = $('<tr>');
+            table.append($('<tbody>').append(tr));
+            for(var c = 0; c < fields.length; c++) {
+                tr.append($('<td>').append($('<div>').text(
+                    structure["records"][i][fields[c]]
+                )));
+            }
+
+        }
+    };
+    };
 
     var create_table_from_structure = function(table, structure) {
         table.empty();
