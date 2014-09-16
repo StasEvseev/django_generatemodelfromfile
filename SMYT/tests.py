a12dce40682e30255122ff260d0e79dc862bddc9
stanislav <evseev@setinbox.com>
2014-09-16 14:05:34 +0400

    Test for filters.

--- SMYT/tests.py
+++ SMYT/tests.py
@@ -34,14 +34,28 @@ class ModelsTestCase(TestCase):
     COUNT = 10
 
     def setUp(self):
+        self.models_data = {}
         for sch_mdl in SCHEME_MODELS:
             for x in xrange(ModelsTestCase.COUNT):
                 data = {}
                 fiels = SCHEME_MODELS.get(sch_mdl)['fields']
                 for fld in fiels:
                     data[fld['id']] = generate_data(fld['type'])
-                MODELS.get(sch_mdl).objects.create(**data)
+                Model = MODELS.get(sch_mdl)
+                if x == 0:
+                    self.models_data[sch_mdl] = data
+
+                Model.objects.create(**data)
+
+    def get_models_data(self, model):
+        return self.models_data[model]
 
     def test_models_count(self):
         for sch_mdl in SCHEME_MODELS:
             self.assertEqual(MODELS.get(sch_mdl).objects.count(), ModelsTestCase.COUNT)
+
+    def test_query(self):
+        for sch_mdl in SCHEME_MODELS:
+            Model = MODELS.get(sch_mdl)
+            data = self.get_models_data(sch_mdl)
+            self.assertIsNotNone(Model.objects.filter(**data))
